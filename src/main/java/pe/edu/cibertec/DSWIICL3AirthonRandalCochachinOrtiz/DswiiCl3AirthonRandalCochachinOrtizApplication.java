27682a8deeb72bf4287a6be87e3fdaf0880cd0dd
Airthon1234 <cipoteland@gmail.com>
2023-11-25 21:31:56 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWIICL3AirthonRandalCochachinOrtiz/DswiiCl3AirthonRandalCochachinOrtizApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWIICL3AirthonRandalCochachinOrtiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3AirthonRandalCochachinOrtizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3AirthonRandalCochachinOrtizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWIICL3AirthonRandalCochachinOrtiz/DswiiCl3AirthonRandalCochachinOrtizApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWIICL3AirthonRandalCochachinOrtiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3AirthonRandalCochachinOrtizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
